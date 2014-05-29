a89e9f16fb854712601d7215c010c9ef4a013e26
Alec Gonzales <alec.gonzales@orangeandbronze.com>
2014-05-29 16:02:26 +0800

    Added third sysout

--- src/com/onb/main/Main.java
+++ src/com/onb/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Git training");
 		System.out.println("second println");
+		System.out.println("third println");
 	}
 
 }
