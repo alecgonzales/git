2501180e7eea6831f7e85115cf300cf4bbae0923
Alec Gonzales <alec.gonzales@orangeandbronze.com>
2014-05-29 15:41:37 +0800

    Revert "added another sysout"
    This reverts commit 02b64aa335acc25596461af0d835dbf1806c9f1e.

--- src/com/onb/main/Main.java
+++ src/com/onb/main/Main.java
@@ -5,6 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Git training");
 		System.out.println("second println");
-		System.out.println("yey new branch");
 	}
+
 }
